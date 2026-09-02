06a6eeb365e37796b93e17fb8894848b44617bd3
Virginie RACAPE <virginei.racape@pokapok.org>
2026-09-02 16:19:13 +0200

    add ;

--- doxy_corr/DOXY_corr_compute.m
+++ doxy_corr/DOXY_corr_compute.m
@@ -325,7 +325,7 @@ elseif ismember(Work.whichCorr,{'INAIR'})
         CORR.po2_inair(isok) = beta(2,1)*Work.surface.argo.pO2_water(isok) +(1-beta(2,1))/(1+beta(1,1)/100) *Work.surface.argo.pO2_air(isok) ;
         slopefun=@(beta,X)(1)./(1+beta/100).*X; 
         [beta2,mdl2] = nlinfitci(CORR.ncep_pO2_scaled(isok)',CORR.po2_inair(isok)',slopefun,[0]);
-        beta(1,1) = beta(1,1) * beta2(1,1) 
+        beta(1,1) = beta(1,1) * beta2(1,1); 
     
     elseif Work.isokC==0
         slopefun=@(beta,X)(1)./beta.*X; %--MG
